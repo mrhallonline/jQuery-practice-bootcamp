f9b7020534b433a10c2d44915a526504ff34af23
Kevin <mrhallonline@gmail.com>
2019-10-13 22:56:52 -0500

    completed jQuery practice lessons

--- index.js
+++ index.js
@@ -67,13 +67,16 @@ $("h1").append("<button>New</button>");
 
 // Define Animations:
 // Change Opacity
-$("button").on("click", function(){ 
-    $("h1").animate({opacity: 0.5});
+// $("button").on("click", function(){ 
+//     $("h1").animate({opacity: 0.5});
+
+// });
+// // Animate Margin
+// $("h1").animate({margin: 20});
+// $("h1").animate({margin: "20%"});
+
+// Chaining Animations
+$("button").on("click", function(){
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
 
 });
-// Animate Margin
-$("h1").animate({margin: 20});
-$("h1").animate({margin: "20%"});
-
-Chaining Animations
-$("h1")
